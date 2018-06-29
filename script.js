0819ed49304ffaa1abe87443d356d49087c9619e
Matthew_Yu <matthewjkyu@.gmail.com>
2018-06-29 17:52:17 -0500

    started preliminary version of prod, script.js holds basic sound functions, edit to obtain data and manipulate. Then modularize and prepare for visualization. script-old deprecated and archived, sketch.js for basic graphic testing, three.js and three-js-test.html deprecated and archived. index.html current html version, barebones empty.

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-function setupWebAudio(){
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var analyserNode = audioCtx.createAnalyser();
-  var source = audioCtx.createMediaElementSource(thisSong);
-  source.connect(analyserNode);
-  source.connect(audioCtx.destination);
-  audio.play();
+/*Script.js - functions for mp3 visualizer using p5.js
+Matthew Yu and Ryan Menghani*/
+
+function preload(){
+  soundFormats('ogg', 'mp3');
+  sound = loadSound('audio/DEMO_1.mp3');
 }
 
-function analyseAudio(){
-  analyserNode.fftSize = 2048;
-  var bufferLength = analyserNode.frequencyBinCount;
-  var frequency = new Uint8Array(bufferLength);
-  var waveform = new Float32Array(bufferLength);
-  analyserNode.getByteFrequencyData(frequency);
-  analyserNode.getFloatTimeDomainData(waveform);
+function setup(){
+  var cnv = createCanvas(800,400);
+  cnv.parent('myContainer');
+  cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(0.2);
 }
 
-function draw(mode){ //adjust draw based on mode
+function draw(){
+  background(0);
 
+  var spectrum = fft.analyze();
+  noStroke();
+  fill(0,255,0); // spectrum is green
+  for (var i = 0; i< spectrum.length; i++){
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h )
+  }
+
+  var waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255,0,0); // waveform is red
+  strokeWeight(3);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map( waveform[i], -1, 1, 0, height);
+    vertex(x,y);
+  }
+  endShape();
+
+  text('click to play/pause', 4, 10);
+}
+
+// fade sound if mouse is over canvas
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
 }
---
+++ sketch.js
@@ -0,0 +1,14 @@
+/*sketch.js*/
+/*testing purposes for learning interplay between HTML and JS*/
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
