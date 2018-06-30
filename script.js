b8056c6a87aa8f30f7db88f7288c21e0656253f3
Matthew_Yu <matthewjkyu@.gmail.com>
2018-06-30 16:21:58 -0500

    added switch song functionality to button, title switch functionality top of page, added css stylesheet, frequency histogram numbers to the top of canvas. TODO: observe general demarcations, create functionality for uploading songs and reading title from them

--- script.js
+++ script.js
@@ -5,26 +5,16 @@ function preload() {
   soundFormats('ogg', 'mp3');
   sound = loadSound('audio/DEMO_1.mp3');
   setting = 1;
-}
 
-function changeSong() {
-  sound.pause();
-  if(setting == 1) {
-    sound = loadSound('audio/DEMO_2.mp3');
-    setting = 2;
-  }
-  else {
-    sound = loadSound('audio/DEMO_1.mp3');
-    setting = 1;
-  }
-
+  title = "DEMO_1 - Scars by Papa Roach";
+  document.getElementById("song_title").innerHTML = title;
 }
 
 function setup() {
-  var cnv = createCanvas(window.innerWidth,window.innerHeight * .8);
+  var cnv = createCanvas(window.innerWidth * .9, window.innerHeight * .75);
   cnv.parent('myContainer');
   cnv.mouseClicked(togglePlay);
-  fft = new p5.FFT(.95, 1024);
+  fft = new p5.FFT(.95, 64);
   sound.amp(0.8);
 }
 
@@ -36,8 +26,12 @@ function draw() {
   fill(0,255,0); // spectrum is green
   for (var i = 0; i< spectrum.length; i++) {
     var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    var h = -height + map(spectrum[i], 0, 255, height, 60);
     rect(x, height, width / spectrum.length, h );
+  }
+  for(var i = 0; i< 64; i++) {
+    text(i, i*25 + 2, 25);
+    text(spectrum[i], i*25 + 2, 50);
   }
 
   var waveform = fft.waveform();
@@ -45,9 +39,9 @@ function draw() {
   beginShape();
   stroke(255,0,0); // waveform is red
   strokeWeight(1.5);
-  for (var i = 0; i< waveform.length; i++){
+  for (var i = 0; i< waveform.length; i++) {
     var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
+    var y = map(waveform[i], -1, 1, 0, height);
     vertex(x,y);
   }
   endShape();
@@ -61,5 +55,23 @@ function togglePlay() {
     sound.pause();
   } else {
     sound.loop();
+  }
+}
+
+function changeSong() {
+  sound.pause();
+  if(setting == 1) {
+    sound = loadSound('audio/DEMO_2.mp3');
+    setting = 2;
+
+    title = "DEMO_2 - Gravity by Against The Current";
+    document.getElementById("song_title").innerHTML = title;
+  }
+  else {
+    sound = loadSound('audio/DEMO_1.mp3');
+    setting = 1;
+
+    title = "DEMO_1 - Scars by Papa Roach";
+    document.getElementById("song_title").innerHTML = title;
   }
 }
