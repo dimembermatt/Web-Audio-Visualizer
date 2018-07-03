2e1808b5215301a941124fd3cba21bd72f1758d9
Matthew_Yu <matthewjkyu@.gmail.com>
2018-07-02 19:41:16 -0500

    updated Script.js with working song transfer and play without typeError, 'magic' hides the 'throw not ready decode', added displayError, loading callbacks for all LoadSound calls

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*Script.js - functions for mp3 visualizer using p5.js
 Matthew Yu and Ryan Menghani*/
 var soundFile;
+var isLoaded;
 
 //load DEMO song and title
 function preload() {
@@ -19,6 +20,7 @@ function setup() {
   fft = new p5.FFT(.95, 64);
 }
 
+//mainLoop
 function draw() {
   background(0);
 
@@ -57,27 +59,39 @@ function togglePlay() {
     soundFile.play();
   }
 }
+
 //return callback if something is dropped onto canvas
 function makeCanvasDroppable(canvas, callback) {
   var e = getElement(canvas.elt.id);
   e.drop(callback);
 }
-//set soundFile
+
+//callback function when recieved dropped file, sets soundFile
 function gotFile(file) {
   soundFile.dispose();
   document.getElementById("song_title").innerHTML = "loading...";
+  soundFile = loadSound(file, null, displayError(), loading());
   title = "local file";
-  setTimeout(function(){
-    soundFile = loadSound(file);
-  }, 5000);
+  document.getElementById("song_title").innerHTML = title;
+  togglePlay();
+}
+
+//button call function that resets song back to DEMO
+function changeSong() {
+  soundFile.dispose();
+  soundFile = loadSound('audio/DEMO_1.mp3', null, displayError(), loading());
+  title = "DEMO_1 - Scars by Papa Roach";
   document.getElementById("song_title").innerHTML = title;
 }
 
-function changeSong() {
-  soundFile.dispose();
-  soundFile = loadSound('audio/DEMO_1.mp3');
-  title = "DEMO_1 - Scars by Papa Roach";
-  document.getElementById("song_title").innerHTML = title;
+//callback function for loadSound
+function displayError() {
+  document.getElementById("song_title").innerHTML = "an Error has occured.";
+}
+
+//callback function for loadSound
+function loading(percent) {
+  document.getElementById("song_title").innerHTML = "loading..." + (percent * 100) + '%';
 }
 
 /**
