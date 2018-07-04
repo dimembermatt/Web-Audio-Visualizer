7701ee715340b9c0cb7b848fdb8f1c4811ce3040
Matthew_Yu <matthewjkyu@.gmail.com>
2018-07-03 21:50:54 -0500

    simple canvas collision detection, object collision deemed cpu intensive until further research.

--- Particle.js
+++ Particle.js
@@ -1,15 +1,17 @@
 //Simple Particle Visualization
-var cols = 100;
-var rows = 50;
+var modifier = 4 / 5;
+var cols =  100 * modifier;
+var rows =  50 * modifier;
+
 
 var particles = create2dArray(cols, rows); //array of arrays, each index of the outer is height, inner is width
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  //setup a system of particles, one for each 1% block of the window
-  var widthSlice = window.innerWidth * .01 / 1;
-  var heightSlice = window.innerHeight * .02 / 1;
+  //setup a system of particles, displayed evenly determined by a modifier
+  var widthSlice = window.innerWidth * .01 * 1/modifier;
+  var heightSlice = window.innerHeight * .02 * 1/modifier;
   //populate 2d array particles with particle objects
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
@@ -21,9 +23,11 @@ function setup() {
 function draw(){
   background(0);
   for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
+    for (var j = 0; j < particles[i].length; j++) {
       particles[i][j].update();
       particles[i][j].show();
+      if(particles[i][j].finished() <= 0)
+        particles[i].splice(j, 1);
     }
   }
 }
@@ -35,19 +39,30 @@ class Particle {
     this.vx = random(-1, 1);
     this.vy = random(-1, 1);
     this.alpha = 255;
+    this.vAlpha = random(-5, 2);
     //this.color = random(0, 255);
   }
+
   update() {
     this.x += this.vx;
     this.y += this.vy;
-    this.alpha -= 1;
+    //wall collision
+    if(this.x < 0 || this.x > window.innerWidth)
+      this.vx = -this.vx;
+    if(this.y < 0 || this.y > window.innerHeight)
+      this.vy = -this.vy;
+
+    this.alpha += this.vAlpha;
   }
+
   show() {
     noStroke();
-    //stroke(255);
     fill(255, this.alpha);
-    //fill(this.color);
     ellipse(this.x, this.y, 3);
+  }
+
+  finished() {
+    return this.alpha;
   }
 }
 
